1ca4c55f39249c7525c81bfdeb26b83cbf81e6d1
Marcellinus Jerricho <marcellinus.jerricho@u.yale-nus.edu.sg>
2022-01-04 17:21:23 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDtrl0wwAyXj5EfWU78BmOYpkryNbnFGiw",
+    authDomain: "birdy-de2d0.firebaseapp.com",
+    projectId: "birdy-de2d0",
+    storageBucket: "birdy-de2d0.appspot.com",
+    messagingSenderId: "1064381062259",
+    appId: "1:1064381062259:web:6d1b8e49c9116ee7035e06",
+    measurementId: "G-PQ0LKF5EY8"
+  };
